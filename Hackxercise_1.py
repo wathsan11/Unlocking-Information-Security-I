052e1593a10d40c1ffe1d55af5540b5927a74cec
Wathsan Induwara <wathsaninduwara@gmail.com>
2026-03-19 01:46:43 +0530

    Update and rename Hackxercise_1 to Hackxercise_1.py

--- Hackxercise_1.py
+++ Hackxercise_1.py
@@ -1,3 +1,6 @@
+# The function check_password(password) is used by a safe with 4-digits passwords, and is susceptible to timing attacks. More specifically, it takes it around 0.1 seconds to check one digit – so brute-forcing all the possible combinations will take more than an hour. Can you implement a way to crack its password in less than a minute?
+
+
 import time
 import sys # ignore
 sys.path.insert(0,'.') # ignore
